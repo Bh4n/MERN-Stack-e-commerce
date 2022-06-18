df94d4d849661709e4262f5b5a7b919d2ebdf1be
Bh4n <bahancan@gmail.com>
2022-06-18 11:36:16 +0300

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,31 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import LinkContainer from 'react-router-bootstrap/LinkContainer';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <Link to="/">E-Commerce</Link >
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>e-commerce</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+          
         </header>
         <main>
+          <Container>
           <Routes>
             <Route path="products/:slug" element={<ProductScreen />} />
             <Route path="/" element={<HomeScreen />} />
           </Routes>
+        </Container>
         </main>
       </div>
     </BrowserRouter>
